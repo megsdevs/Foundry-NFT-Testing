4c7df7276b944c39a9daa3369888308aa2678f2d
Megan Lloyd-Palmer <mlloydpalmer@outlook.com>
2023-01-18 19:16:44 +0300

    add working demo tests

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 ## Run tests
 
-Run tests on fork of mainnet:
+Run tests on fork of mainnet.
+
+### Working Test 
+which doesn't use NFT balanceOf(address) function:
 ```
-forge test --fork-url https://rpc.ankr.com/eth                   
+forge test --fork-url https://rpc.ankr.com/eth --match-contract ShellzOrbWorkingTest
 ```
+sample output:
+```
+Running 4 tests for test/ShellzOrb.t.sol:ShellzOrbWorkingTest
+[PASS] testBalanceOf() (gas: 125600)
+[PASS] testImmutableVariables() (gas: 26613)
+[PASS] testOnlyInitializeOnce() (gas: 18230)
+[PASS] testOwner() (gas: 24760)
+```
+
+### Working Test 2
+which deploys new contract instances:
+```
+forge test --fork-url https://rpc.ankr.com/eth --match-contract ShellzOrbWorkingTest2
+```
+sample output:
+```
+Running 4 tests for test/ShellzOrb.t.sol:ShellzOrbWorkingTest2
+[PASS] testBalanceOf() (gas: 927405)
+[PASS] testImmutableVariables() (gas: 26481)
+[PASS] testOnlyInitializeOnce() (gas: 18197)
+[PASS] testOwner() (gas: 24727)
+Test result: ok. 4 passed; 0 failed; finished in 4.33s
+```
+
+### Test With Issue
+
+```
+forge test --fork-url https://rpc.ankr.com/eth --match-contract ShellzOrbTest       
+```
+
+test hangs indefinitely after compilation:
+```
+[⠒] Compiling...
+[⠊] Compiling 1 files with 0.8.17
+[⠢] Solc 0.8.17 finished in 5.47s
+Compiler run successful
+
+```
+
+Only tests which use balanceOf function on already deployed mainnet instances of the NFT contracts fail in this way. Removing any balanceOf calls or deploying new instances as demonstrated in the above working tests gets around the issue. 
--- test/ShellzOrb.t.sol
+++ test/ShellzOrb.t.sol
@@ -9,9 +9,8 @@ import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.so
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
 
-contract ShellzOrbUpgradeTest is Test {
-    // proxy admin contract to control proxy 
-    // see https://docs.openzeppelin.com/contracts/3.x/api/proxy#ProxyAdmin
+contract ShellzOrbTest is Test {
+    // proxy admin contract to control proxy, see https://docs.openzeppelin.com/contracts/3.x/api/proxy#ProxyAdmin
     // mainnet impl v2 contract
     ShellzOrbV2 implementation = ShellzOrbV2(address(0x0a1618A6F0b9D3E37DC673AB1cd67471760bC06A));
     // mainnet admin contract
@@ -29,20 +28,8 @@ contract ShellzOrbUpgradeTest is Test {
     uint256 beforeBalance;
 
     function setUp() external {
-        // // act as contract owner
-        // vm.startPrank(jamieson);
-
-        // // deploy new contracts to get around rpc issue
-        // implementation = new ShellzOrb();
-        // proxy = new TransparentUpgradeableProxy(address(implementation), address(admin), "");
-        
         // wrap in ABI to support easier calls
         wrappedProxy = ShellzOrbV2(address(proxy));
-
-        // wrappedProxy.initialize();  // dont use if using ready initialized mainnet contracts
-        // wrappedProxy.devMint(topHolder, 250);  // dont use if using real mainnet contracts as already at max supply
-
-        // vm.stopPrank();
     }
 
     function testOwner() public {
@@ -73,5 +60,125 @@ contract ShellzOrbUpgradeTest is Test {
         console.log("Balance of top holder after mint 4: %s", afterBalance);
         assertEq(afterBalance-beforeBalance, 4);
     }
+}
 
+contract ShellzOrbWorkingTest is Test {
+    // proxy admin contract to control proxy, see https://docs.openzeppelin.com/contracts/3.x/api/proxy#ProxyAdmin
+    // mainnet impl v2 contract
+    ShellzOrbV2 implementation = ShellzOrbV2(address(0x0a1618A6F0b9D3E37DC673AB1cd67471760bC06A));
+    // mainnet admin contract
+    ProxyAdmin admin = ProxyAdmin(address(0x523422E7671b1F4Cb148ec5BC8848d6175b8Dcb0));
+    // mainnet proxy contract
+    TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(address(0xe17827609Ac34443B3987661f4e037642F6BD9bA)));
+
+    ShellzOrbV2 wrappedProxy;
+
+    // mainnet EOAs
+    address jamieson = address(0x3b6312d78Bc3Bb215842D054627c3A2B22f2bd9c);
+    address topHolder = address(0xea831d27E0E52b4dC32F42A5c4C5cBfd9b6D4a58);  // holyshellzbatman.eth
+
+    uint256 afterBalance;
+    uint256 beforeBalance;
+
+    function setUp() external {
+        // wrap in ABI to support easier calls
+        wrappedProxy = ShellzOrbV2(address(proxy));
+    }
+
+    function testOwner() public {
+        assertEq(wrappedProxy.owner(), address(jamieson));
+        assertEq(admin.owner(), address(jamieson));
+    }
+
+    function testOnlyInitializeOnce() public {
+        vm.expectRevert("Initializable: contract is already initialized");
+        wrappedProxy.initialize();
+    }
+
+    function testImmutableVariables() public {
+        assertEq(wrappedProxy.name(), "Shellz Orb");
+        assertEq(wrappedProxy.symbol(), "SHELLZ");
+        assertEq(wrappedProxy.getMaxLaunchpadSupply(), 1000);
+        assertEq(wrappedProxy.supportsInterface(type(IERC721Upgradeable).interfaceId), true);
+    }
+
+    function testBalanceOf() public {
+        // beforeBalance = wrappedProxy.balanceOf(topHolder);  // hangs here if use real mainnet contracts
+        beforeBalance = 300;
+        console.log("Balance of top holder: %s", beforeBalance);
+
+        vm.prank(wrappedProxy.launchpad());
+        wrappedProxy.mintTo(topHolder, 4);
+
+        // afterBalance = wrappedProxy.balanceOf(topHolder);  // hangs here if use real mainnet contracts
+        afterBalance = 304;
+        console.log("Balance of top holder after mint 4: %s", afterBalance);
+        assertEq(afterBalance-beforeBalance, 4);
+    }
+}
+
+
+contract ShellzOrbWorkingTest2 is Test {
+    // proxy admin contract to control proxy, see https://docs.openzeppelin.com/contracts/3.x/api/proxy#ProxyAdmin
+    // mainnet impl v2 contract
+    ShellzOrbV2 implementation = ShellzOrbV2(address(0x0a1618A6F0b9D3E37DC673AB1cd67471760bC06A));
+    // mainnet admin contract
+    ProxyAdmin admin = ProxyAdmin(address(0x523422E7671b1F4Cb148ec5BC8848d6175b8Dcb0));
+    // mainnet proxy contract
+    TransparentUpgradeableProxy proxy = TransparentUpgradeableProxy(payable(address(0xe17827609Ac34443B3987661f4e037642F6BD9bA)));
+
+    ShellzOrbV2 wrappedProxy;
+
+    // mainnet EOAs
+    address jamieson = address(0x3b6312d78Bc3Bb215842D054627c3A2B22f2bd9c);
+    address topHolder = address(0xea831d27E0E52b4dC32F42A5c4C5cBfd9b6D4a58);  // holyshellzbatman.eth
+
+    uint256 afterBalance;
+    uint256 beforeBalance;
+
+    function setUp() external {
+        // act as contract owner
+        vm.startPrank(jamieson);
+
+        // deploy new contracts to get around rpc issue
+        implementation = new ShellzOrbV2();
+        proxy = new TransparentUpgradeableProxy(address(implementation), address(admin), "");
+        
+        // wrap in ABI to support easier calls
+        wrappedProxy = ShellzOrbV2(address(proxy));
+
+        wrappedProxy.initialize();  // dont use if using ready initialized mainnet contracts
+        wrappedProxy.devMint(topHolder, 250);  // dont use if using real mainnet contracts as already at max supply
+
+        vm.stopPrank();
+    }
+
+    function testOwner() public {
+        assertEq(wrappedProxy.owner(), address(jamieson));
+        assertEq(admin.owner(), address(jamieson));
+    }
+
+    function testOnlyInitializeOnce() public {
+        vm.expectRevert("Initializable: contract is already initialized");
+        wrappedProxy.initialize();
+    }
+
+    function testImmutableVariables() public {
+        assertEq(wrappedProxy.name(), "Shellz Orb");
+        assertEq(wrappedProxy.symbol(), "SHELLZ");
+        assertEq(wrappedProxy.getMaxLaunchpadSupply(), 1000);
+        assertEq(wrappedProxy.supportsInterface(type(IERC721Upgradeable).interfaceId), true);
+    }
+
+    function testBalanceOf() public {
+        beforeBalance = wrappedProxy.balanceOf(topHolder);  // hangs here if use real mainnet contracts
+        console.log("Balance of top holder: %s", beforeBalance);
+
+        vm.prank(wrappedProxy.launchpad());
+        wrappedProxy.mintTo(topHolder, 4);
+
+        afterBalance = wrappedProxy.balanceOf(topHolder);  // hangs here if use real mainnet contracts
+        console.log("Balance of top holder after mint 4: %s", afterBalance);
+        assertEq(afterBalance-beforeBalance, 4);
+    }
 }
